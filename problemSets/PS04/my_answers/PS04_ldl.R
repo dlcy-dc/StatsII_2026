25674507cb9db414cf18ec5d02ae535cbaa70989
dlcy-dc <delaceyl@tcd.ie>
2026-04-10 23:58:16 +0100

    PS04

--- problemSets/PS04/my_answers/PS04_ldl.R
+++ problemSets/PS04/my_answers/PS04_ldl.R
@@ -27,10 +27,11 @@ pkgTest <- function(pkg){
 # ex: stringr
 # lapply(c("stringr"),  pkgTest)
 
-lapply(c("eha", "nnet", "MASS"),  pkgTest)
+lapply(c("eha", "nnet", "MASS", "dplyr", "survival", "survminer", "stargazer", "sampleSelection"),  pkgTest)
 
 # set wd for current folder
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
 
 #####################
 # Problem 1
@@ -38,6 +39,33 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 # load data on child mortality by mother's background and child gender
 data("child")
+summary(child)
+str(child)
+table(child$sex)
+
+# response var construction vars (time, time2, event) -> (enter, exit, event)
+# m.age = mother age
+# sex = infant gender
+
+
+# base model - build cox regression
+
+child_surv <- coxph(Surv(enter, exit, event) ~ m.age + sex, data = child)
+summary(child_surv)
+
+#check model fit
+drop1(child_surv, test = "Chisq")
+
+#hazard rate percentage for girls
+1 - 0.921074
+
+
+# print to stargazer
+stargazer(child_surv,
+          type = "latex",
+          title = "Hazard Rate Of Child Mortality: Mother's Age and Sex of Child",
+          dep.var.labels=c("Hazard rate of child mortatlity"),
+          covariate.labels=c("Mother's Age", "Sex of Child"))
 
 #####################
 # Problem 2
@@ -45,3 +73,52 @@ data("child")
 
 # load data
 disaster_data <- read.csv("https://raw.githubusercontent.com/ASDS-TCD/StatsII_2026/refs/heads/main/datasets/disaster_response.csv")
+str(disaster_data)
+names(disaster_data)
+summary(disaster_data)
+
+# outcome variables
+# selection equation = binContribution - binary, is a contribution made?
+# outcome equation =  = originalContributionMillionUSDLogged - amount of relief that is provided
+
+# Input variables #
+# occurrences
+# deathsEM
+# normalizedDamageEMLogged
+
+# ensure outcome variables are appropriately coded
+
+unique(disaster_data$binContribution)
+# outcome 1 is suitable for model
+
+sum(is.na(disaster_data$originalContributionMillionUSDLogged))
+# outcome 2 needs to have contribution's of 0 recoded to NA,
+# Heckmann model needs this to allow it to interpret values as censored.
+# outcome model is conditional on selection model - the value is 0 because they recieved no aid - but we want to estimate what it may have been.
+
+disaster_data <- disaster_data %>%
+  mutate(originalContributionMillionUSDLogged = ifelse(binContribution == 0, NA, originalContributionMillionUSDLogged))
+
+# check again for NAs and that data is largely in order
+sum(is.na(disaster_data$originalContributionMillionUSDLogged))
+
+unique(disaster_data$originalContributionMillionUSDLogged)
+
+# run heckmann model
+heckmann <- heckit(selection = binContribution ~ occurrences + deathsEM + normalizedDamageEMLogged,
+       outcome = originalContributionMillionUSDLogged ~ occurrences + deathsEM + normalizedDamageEMLogged,
+       data = disaster_data)
+summary(heckmann)
+
+#create stargazer table for probit
+stargazer(heckmann,
+          type = "latex",
+          selection.equation = TRUE,
+          column.labels = c("Disaster Relief Provision"),
+          title = "Selection Model")
+
+stargazer(heckmann,
+          type = "latex",
+          selection.equation = FALSE,
+          column.labels = c("Logged Contribution (in Million USD)"),
+          title = "Outcome Model")
